81284290399650e6727996f864f41f0cdfecdb31
craftwerk <pst@crftwrk.de>
2019-06-11 10:57:57 +0200

    Version 1.0.1

--- functions.php
+++ functions.php
@@ -1,11 +1,19 @@
 <?php
-/**
-* Child theme stylesheet einbinden in Abhängigkeit vom Original-Stylesheet
-*/
+	// Style CSS
+	function child_theme_styles() {
+		wp_enqueue_style( 'parent-style', get_template_directory_uri() . '/style.css' );
+		wp_enqueue_style( 'child-theme-css', get_stylesheet_directory_uri() .'/style.css' , array('parent-style'));
 
-function child_theme_styles() {
-wp_enqueue_style( 'parent-style', get_template_directory_uri() . '/style.css' );
-wp_enqueue_style( 'child-theme-css', get_stylesheet_directory_uri() .'/style.css' , array('parent-style'));
+	}
+	add_action( 'wp_enqueue_scripts', 'child_theme_styles' );
+?>
 
-}
-add_action( 'wp_enqueue_scripts', 'child_theme_styles' );?>
+
+<?php
+	// Custom JS
+	function child_add_scripts() {
+		wp_register_script('custom', get_stylesheet_directory_uri() . '/js/custom.js', false, '', true);
+		wp_enqueue_script('custom');
+	}
+	add_action( 'wp_enqueue_scripts', 'child_add_scripts' ); 
+?>
